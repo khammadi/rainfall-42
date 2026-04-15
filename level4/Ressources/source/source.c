dcb7c3127e3831197e58b7a385661db0fcc977b8
Khadija Hammadi <khammadi@c4r10p2.1337.ma>
2026-04-15 16:10:29 +0100

    after analyse in assembly now we add n function in c language

--- level4/Ressources/source/source.c
+++ level4/Ressources/source/source.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+void n(void)
+{
+  char buffer[0x200];
+
+  fgets(buffer, 0x200, stdin); 
+  p(buffer);
+  if (m == 0x1025544) {
+    system("/bin/cat /home/user/level5/.pass");
+  }
+}
 
 int main(int argc, char *argv[])
 {
